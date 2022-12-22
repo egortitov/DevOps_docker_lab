4ed12f73f5038366865045483fa68c46cd6456cf
egortitov <70881645+egortitov@users.noreply.github.com>
2022-12-22 08:38:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const http = require('http');
-
-const hostname = '0.0.0.0';
+const express = require('express');
+const app = express();
+const hostname = "0.0.0.0";
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('DevOps, docker-lab');
-});
+//Create HTTP server and listen on port 3000 for requests
+app.get('/', (req, res) => {
+	res.statusCode = 200;
+	res.send('Hello World!');
+})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/foo', (req, res) => {
+	res.send('footest');
+})
+
+app.get("/healthz", (req, res) => res.status(200).json({ status: "ok" }));
+
+//listen for request on port 3000, and as a callback function have the port listened on logged
+app.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`);
 });
